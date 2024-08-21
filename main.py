d58ab9cca58a9255949a2f9638edcfdb3ec509c7
DeograciasAdanlokonon <adanlokonondeogracias1@gmail.com>
2024-08-21 08:33:28 +0100

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, URLField, SelectField, TimeField, SubmitField
@@ -28,7 +28,7 @@ class CafeForm(FlaskForm):
     location = URLField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
     open = TimeField('Opening Time e.g. 8AM', validators=[DataRequired()])
     close = TimeField('Closing Time e.g. 5.30PM', validators=[DataRequired()])
-    coffe = SelectField('Coffe Rating', choices=[('☕️', '☕️'), ('☕️☕️', '☕️☕️'), ('☕️☕️☕️', '☕️☕️☕️'), ('☕️☕️☕️☕️', '☕️☕️☕️☕️'),
+    coffee = SelectField('Coffe Rating', choices=[('☕️', '☕️'), ('☕️☕️', '☕️☕️'), ('☕️☕️☕️', '☕️☕️☕️'), ('☕️☕️☕️☕️', '☕️☕️☕️☕️'),
                                                  ('☕️☕️☕️☕️☕️', '☕️☕️☕️☕️☕️')], validators=[DataRequired()])
     wifi = SelectField('Wifi Strength Rating', choices=[('✘', '✘'), ('💪', '💪'), ('💪💪', '💪💪'), ('💪💪💪', '💪💪💪'),
                                                         ('💪💪💪💪', '💪💪💪💪'), ('💪💪💪💪💪', '💪💪💪💪💪')], validators=[DataRequired()])
@@ -43,15 +43,40 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add', methods=['GET', 'POST'])
-def add_cafe():
+# @app.route('/add', methods=['GET', 'POST'])
+# def add_cafe():
+#     form = CafeForm()
+#
+#     if form.validate_on_submit():
+#         cafe_name = form.cafe.data
+#         location = form.location.data
+#         open_time = form.open.data
+#         close_time = form.close.data
+#         coffe_rate = form.coffee.data
+#         wifi_rate = form.wifi.data
+#         power_rate = form.power.data
+#
+#         # Make the form write a new row into cafe-data.csv
+#         # with if form.validate_on_submit()
+#         with open('cafe-data.csv', 'a', encoding='utf-8') as csv_file:
+#             csv_file.write(f"\n{cafe_name}, {location}, {open_time}, {close_time}, "
+#                            f"{coffe_rate}, {wifi_rate}, {power_rate}")
+#
+#             return redirect('cafes')
+#
+#     return render_template('add.html', form=form)
+
+
+@app.route('/cafes', methods=['GET', 'POST'])
+def cafes():
     form = CafeForm()
+
     if form.validate_on_submit():
         cafe_name = form.cafe.data
         location = form.location.data
         open_time = form.open.data
         close_time = form.close.data
-        coffe_rate = form.coffe.data
+        coffe_rate = form.coffee.data
         wifi_rate = form.wifi.data
         power_rate = form.power.data
 
@@ -60,19 +85,18 @@ def add_cafe():
         with open('cafe-data.csv', 'a', encoding='utf-8') as csv_file:
             csv_file.write(f"\n{cafe_name}, {location}, {open_time}, {close_time}, "
                            f"{coffe_rate}, {wifi_rate}, {power_rate}")
-            return cafes()
 
-    return render_template('add.html', form=form)
+            flash('Data successfully inserted', 'success')  # send a success message
+            return redirect('cafes')
 
-
-@app.route('/cafes')
-def cafes():
+    # Here we read cafe-data.csv and get all rows inside to display
     with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+
+    return render_template('cafes.html', cafes=list_of_rows, form=form)
 
 
 if __name__ == '__main__':
